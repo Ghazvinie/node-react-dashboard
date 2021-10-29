61c8925df28c119c7f4e797ec981946ab7f27a7b
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-10-29 19:40:05 +0100

    setting up apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import CPU from './components/CPU';
 
 function App() {
@@ -7,22 +7,23 @@ function App() {
 
   // Apollo Setup
   const client = new ApolloClient({
-    uri:'http://localhost:4000/graphql',
+    uri: 'http://localhost:4000/graphql',
     cache: new InMemoryCache()
+  });
 
-  })
+
+
+
 
   return (
     <ApolloProvider client={client}>
-
-
-    <div className='dashboard-container'>
-    <CPU/>
-    <CPU/>
-    <CPU/>
-    <CPU/>
-    <CPU/>
-    </div>
+      <div className='dashboard-container'>
+        <CPU />
+        <CPU />
+        <CPU />
+        <CPU />
+        <CPU />
+      </div>
     </ApolloProvider>
   );
 }
--- client/src/components/CPU.js
+++ client/src/components/CPU.js
@@ -1,11 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-export default function CPU(){
+export default function CPU() {
 
-    
+
+    const testQuery = gql`{
+      testData(id: 2) {
+        name,
+        number
+      }
+    }`;
+
+    const {loading, error, data} = useQuery(testQuery)
+    console.log(error, data, loading)
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+        console.log(error)
+
     return (
         <div>
             CPU
+            {data.testData.name}
         </div>
     )
 }
