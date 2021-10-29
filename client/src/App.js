10c60c1154dff6901637ece38a9b1b35d6166ef3
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-10-29 19:18:44 +0100

    setting up apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import CPU from './components/CPU';
 
 function App() {
+
+
+  // Apollo Setup
+  const client = new ApolloClient({
+    uri:'http://localhost:4000/graphql',
+    cache: new InMemoryCache()
+
+  })
+
   return (
+    <ApolloProvider client={client}>
+
+
     <div className='dashboard-container'>
     <CPU/>
     <CPU/>
@@ -10,6 +23,7 @@ function App() {
     <CPU/>
     <CPU/>
     </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/CPU.js
+++ client/src/components/CPU.js
@@ -1,25 +1,11 @@
 import React, {useEffect, useState} from 'react';
 
 export default function CPU(){
-    const [data, setData] = useState();
-
-    useEffect(() => {
-        const connectToBE = async () => {
-            const res = await fetch('/api', {
-                method: 'GET',
-            });
-            const parsedRes = await res.json();
-            setData(parsedRes);
-        };
-        connectToBE();
-
-    }, [])
 
     
     return (
         <div>
             CPU
-            {data}
         </div>
     )
 }
